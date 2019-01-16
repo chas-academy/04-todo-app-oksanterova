46430e285782946ff7011c62f039545c567e974d
oksanterova <oksana.kanterova@gmail.com>
2019-01-16 19:27:07 +0100

    Change credentials to database

--- config/credentials.php
+++ config/credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'todo-app-db');
+define('DB_HOST', 'my73b.sqlserver.se');
+define('DB_USER', '235239_hy26762');
+define('DB_PASS', 'nhjrflthj');
+define('DB_NAME', '235239-todo-app-db');
